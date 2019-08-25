ff340d655a337a073be794823b2e2f8e516cb6f4
zahra <fz_chabanechaouch@esi.dz>
2019-08-25 16:56:28 +0100

    récupérer les news depuis firebase

--- app/src/main/java/com/example/newsfinal/ListNews.kt
+++ app/src/main/java/com/example/newsfinal/ListNews.kt
@@ -11,17 +11,18 @@ import android.support.v7.widget.RecyclerView
 import android.widget.Toast
 import com.example.newsfinal.Interface.ServiceInterface
 import com.example.newsfinal.Services.ServiceVolley
+import com.google.firebase.database.*
 import com.google.gson.Gson
-
-
 import kotlinx.android.synthetic.main.fragment_list_news.*
 import org.json.JSONArray
 
 class ListNews : Fragment() {
 
-    private var listOfNews : List<News>? = listOf()
+    private var listOfNews : MutableList<News> = mutableListOf()
     private var mAdapter: ListNewsAdapter ?= null
     private var categorie: Int = 0
+    private var database : FirebaseDatabase?= null
+    private var myRef: DatabaseReference ?= null
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
@@ -40,8 +41,36 @@ class ListNews : Fragment() {
                 adapter = ListNewsAdapter(listOfNews, { partItem : News  -> partItemClicked(partItem) })
                 mAdapter = adapter as ListNewsAdapter
                 }
-            getListNews(categorie)
+
+            database = FirebaseDatabase.getInstance()
+            myRef = database?.getReference("news/news")
+
+            // Read from the database
+            myRef?.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+
+                    addNewsToLIst(dataSnapshot)
+                    mAdapter?.refreshAdapter(listOfNews as MutableList<News>)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Failed to read value
+                }
+            })
+
+
         }
+
+    fun addNewsToLIst(dataSnapshot: DataSnapshot) {
+        for (child in dataSnapshot.children) {
+            val article = child.getValue(News::class.java)
+            if (article != null) {
+                listOfNews.add(article)
+            }
+        }
+    }
 
     fun getListNews(categorie: Int) {
         val service: ServiceInterface = ServiceVolley()
--- app/src/main/java/com/example/newsfinal/News.kt
+++ app/src/main/java/com/example/newsfinal/News.kt
@@ -5,7 +5,12 @@ import java.io.Serializable
 
 
 data class News(
-                val id: Int? = null,
-                val title: String, val description: String, val date: String, val image: String, val categorie: String, val author: String) : Serializable {
+                var id: String = "",
+                var title: String = "",
+                var description : String = "",
+                var date: String = "",
+                var image: String = "",
+                var categorie: String = "",
+                var author: String = "") : Serializable {
 
 }

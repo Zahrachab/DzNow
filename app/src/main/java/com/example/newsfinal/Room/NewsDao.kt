73b43651c17c64ad9b350539136371f9d9126193
zahra <fz_chabanechaouch@esi.dz>
2019-09-16 21:24:54 +0100

    fix arg0 in query

--- app/src/main/java/com/example/newsfinal/Room/NewsDao.kt
+++ app/src/main/java/com/example/newsfinal/Room/NewsDao.kt
@@ -18,7 +18,7 @@ interface NewsDao {
     @Query("SELECT * FROM News")
     fun getNews (): List<News>
 
-    @Query("SELECT * FROM News WHERE id = :arg0")
+    @Query("SELECT * FROM News WHERE id = :id")
     fun getNewsById(id: Int): News
 
     @Delete
